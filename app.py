b44ee99a43fb9f0424173d6f627595275d8a4274
Akshay G <aksaygts.mit@gmail.com>
2024-06-15 16:16:46 +0530

    data ingestion

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from src.mlpro.logger import logging
 from src.mlpro.exception import CustomException
-# from src.mlproject.components.data_ingestion import DataIngestion
-# from src.mlproject.components.data_ingestion import DataIngestionConfig
+from src.mlpro.components.data_ingestion import DataIngestion
+from src.mlpro.components.data_ingestion import DataIngestionConfig
 # from src.mlproject.components.data_transformation import DataTransformationConfig,DataTransformation
 # from src.mlproject.components.model_tranier import ModelTrainerConfig,ModelTrainer
 
@@ -11,9 +11,10 @@ import sys
 if __name__=="__main__":
     logging.info("The execution has started")
 
-    # try:
+    try:
         # data_ingestion_config=DataIngestionConfig()
-        # data_ingestion=DataIngestion()
+        data_ingestion=DataIngestion()
+        data_ingestion.initiate_data_ingestion()
         # train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
 # 
         # data_transformation_config=DataTransformationConfig()
@@ -26,8 +27,6 @@ if __name__=="__main__":
         # print(model_trainer.initiate_model_trainer(train_arr,test_arr))
         # 
     
-    try:
-        a=1/0
     
     except Exception as e:
         logging.info("Custom Exception")
